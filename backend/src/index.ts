cc5a12e4005b89f3fccbbe3520f36e6eb2032c70
Marzooq13579 <marzooqabs@gmail.com>
2024-01-02 23:14:29 +0530

    Added prisma ORM and Basic User schema to register user

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,37 @@
-
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { PrismaClient, User } from "@prisma/client";
+const prisma = new PrismaClient();
 
 dotenv.config();
 
-const app:Express = express();
+const app: Express = express();
 const port = process.env.PORT;
 
-app.get("/test", (req:Request, res:Response) => {
-  res.send("welcome to social media app server!");
+app.use(express.json());
+
+app.get("/test", (req: Request, res: Response) => {
+  res.send("welcome to private social media app!");
+});
+
+app.post("/register", async (req: Request, res: Response) => {
+  try {
+    const { email, password, role, name } = req.body;
+
+    const createdUser: User = await prisma.user.create({
+      data: {
+        email,
+        password,
+        name,
+        role: role || "USER",
+      },
+    });
+
+    res.status(201).json(createdUser);
+  } catch (err) {
+    console.error("Error Registering user:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.listen(port, () => {
