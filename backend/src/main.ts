1e61be53a95773ea34633375a4e3d51928ff0629
Marzooq13579 <marzooqabs@gmail.com>
2024-08-03 14:44:46 +0530

    configured swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; // Add this line to import DocumentBuilder and SwaggerModule
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['log', 'error', 'warn', 'debug', 'verbose'], // Ensure all log levels are enabled
-  });
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('connectApp')
+    .setDescription('The Real Time Chat API docs')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+      operationsSorter: 'method',
+      tagsSorter: 'alpha',
+    },
+  });
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
