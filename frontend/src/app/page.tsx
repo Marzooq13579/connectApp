e0f8501d3215840814dfa860783a061fbd020afc
marzooqdev <shabuddin.abbas@mileseducation.com>
2024-09-24 15:16:42 +0530

    Logout Functionality

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,14 @@
+import LogoutComponent from "@/components/LogoutComponent";
 import Image from "next/image";
 
 export default function Home() {
+
+
+
   return (
+    <>
     <h1>Welcome to chat App</h1>
+    <LogoutComponent/>
+    </>
   );
 }
---
+++ frontend/src/components/LogoutComponent.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from "react";
+import Cookies from "js-cookie";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+
+const LogoutComponent = () => {
+  const router = useRouter();
+
+  function handleLogout() {
+    Cookies.remove("token");
+    toast.info("Looged Out!");
+    router.push("/login");
+  }
+
+  return (
+    <button type="button" className="btn-primary" onClick={handleLogout}>
+      Logout
+    </button>
+  );
+};
+
+export default LogoutComponent;
